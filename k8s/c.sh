5c49dc5ea3231191871a538f35d639d890b33508
nik786 <chakraborty.rock@gmail.com>
2020-03-18 09:15:22 +0530

    Update c.sh

--- k8s/c.sh
+++ k8s/c.sh
@@ -2,11 +2,11 @@ aws --region us-east-1 ecr get-login-password \
     | docker login \
         --password-stdin \
         --username AWS \
-        "758637906269.dkr.ecr.us-east-1.amazonaws.com"
+        "xxxx.dkr.ecr.us-east-1.amazonaws.com"
 
 
-aws ecr get-login --region us-east-1  | docker login --password-stdin --username AWS  "758637906269.dkr.ecr.us-east-1.amazonaws.com"
+aws ecr get-login --region us-east-1  | docker login --password-stdin --username AWS  "xxxx.dkr.ecr.us-east-1.amazonaws.com"
 
-echo $(aws ecr get-login-password)| docker login --password-stdin --username AWS 758637906269.dkr.ecr.us-east-1.amazonaws.com
+echo $(aws ecr get-login-password)| docker login --password-stdin --username AWS xxxx.dkr.ecr.us-east-1.amazonaws.com
 
-docker login -u AWS -p $(aws ecr get-login-password) https://$(aws sts get-caller-identity --query '758637906269' --output text).dkr.ecr.us-east-1.amazonaws.com
+docker login -u AWS -p $(aws ecr get-login-password) https://$(aws sts get-caller-identity --query 'xxxxx' --output text).dkr.ecr.us-east-1.amazonaws.com
