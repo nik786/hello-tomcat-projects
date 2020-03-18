1a94115733b799f5699daef3dc60953d3d75e2fb
nik786 <chakraborty.rock@gmail.com>
2020-03-18 09:14:10 +0530

    Update c3.sh

--- k8s/c3.sh
+++ k8s/c3.sh
@@ -2,6 +2,6 @@
 SECRET_NAME=us-east-1-ecr-registry
 TOK=`aws ecr --region=us-east-1 get-authorization-token --output text --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2`
 EMAIL=email@email.com
-kubectl create secret docker-registry $SECRET_NAME --docker-server=https://758637906269.dkr.ecr.us-east-1.amazonaws.com --docker-username=AWS  --docker-password="${TOK}"  --docker-email="${EMAIL}" -n testing
+kubectl create secret docker-registry $SECRET_NAME --docker-server=https://xxxxx.dkr.ecr.us-east-1.amazonaws.com --docker-username=AWS  --docker-password="${TOK}"  --docker-email="${EMAIL}" -n testing
 
 kubectl patch serviceaccount default -p '{"imagePullSecrets":[{"name":"'$SECRET_NAME'"}]}' -n testing
